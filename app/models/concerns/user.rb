b9edae4416a46fa6f9ccd4427525fc71df3e931e
Paterson Anaccius <lesage1720@gmail.com>
2018-04-08 06:01:53 +0000

    add user validation

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-    
+    validates :username, presence: true, 
+               uniqueness: {case_sensitive: false},
+               length: 3..25
+    validates :email, presence: true, 
+               uniqueness: {case_sensitive: false},
+               format: /@/
 end
